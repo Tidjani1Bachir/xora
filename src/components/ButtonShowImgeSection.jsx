cf83ae6b2269ca0f82f2a5058169c6d1c8dbe064
Tidjani Bachir <chiro.tidjani@gmail.com>
2024-10-14 18:38:16 +0100

    changes

---
+++ src/components/ButtonShowImgeSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ButtonShowImgeSection = () => {
+  return (
+    <div className="relative flex items-center min-h-[60px] px-4 g4 rounded-2xl inner-before group-hover:before:opacity-100 overflow-hidden">click here</div>
+  )
+}
+
+export default ButtonShowImgeSection
